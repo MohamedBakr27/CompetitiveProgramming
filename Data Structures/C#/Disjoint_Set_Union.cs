432f50307165ff0abc8f99eb318e05158959a334
Mohamed Bakr <91466115+MohamedBakr27@users.noreply.github.com>
2023-09-19 18:14:10 +0300

    Add files via upload

---
+++ Data Structures/C#/Disjoint_Set_Union.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CP
+{
+    public class Disjoint_Set_Union
+    {
+        public int[] parent;
+        public int[] sz;
+        public Disjoint_Set_Union(int n)
+        {
+            parent = new int[n+1];
+            sz = new int[n+1];
+            for(int i = 0; i <= n; i++)
+            {
+                parent[i] = i;
+                sz[i] = 1;
+            }
+        }
+        public int find(int u)
+        {
+            if (u == parent[u]) return u;
+            return parent[u] = find(parent[u]);
+        }
+        public bool add(int u, int v)
+        {
+            u = find(u); v = find(v);
+            if (u == v) return false;
+            if (sz[u] < sz[v])
+            {
+                parent[u] = v;
+                sz[v] += sz[u];
+            }
+            else
+            {
+                parent[v] = u;
+                sz[u] += sz[v];
+            }
+            return true;
+        }
+        public bool same(int u, int v)
+        {
+            return find(u) == find(v);
+        }
+    }
+}
