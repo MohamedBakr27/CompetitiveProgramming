d39c35c5fe7afef17940e6600921cbfb9ac0bf94
Mohamed Bakr <91466115+MohamedBakr27@users.noreply.github.com>
2023-09-19 17:52:06 +0300

    Update Disjoint_Set_Union.java 10

--- Data Structures/Java/Disjoint_Set_Union.java
+++ Data Structures/Java/Disjoint_Set_Union.java
@@ -3,19 +3,19 @@
 
 public class Disjoint_Set_Union 
 {
-    public int[] parent;
-    public int[] sz;
-    public Disjoint_Set_Union(int n)
-    {
-        parent = new int[n+1];
-        sz = new int[n+1];
-        for(int i = 0;i<=n;i++)
-        {
-            parent[i] = i;
-            sz[i] = 1;
-        }
-    }
-    public int find(int u)
+	public int[] parent;
+	public int[] sz;
+	public Disjoint_Set_Union(int n)
+	{
+		parent = new int[n+1];
+		sz = new int[n+1];
+		for(int i = 0;i<=n;i++)
+		{
+			parent[i] = i;
+			sz[i] = 1;
+		}
+	}
+	public int find(int u)
 	{
 		if (u == parent[u]) return u;
 		return parent[u] = find(parent[u]);
@@ -25,14 +25,14 @@ public class Disjoint_Set_Union
 		u = find(u); v = find(v);
 		if (u == v) return false;
 		if (sz[u] < sz[v]) 
-        {
+		{
 			parent[u] = v;
 			sz[v] += sz[u];
 		}
 		else 
-        {
-		    parent[v] = u;
-		    sz[u] += sz[v];
+		{
+			parent[v] = u;
+			sz[u] += sz[v];
 		}
 		return true;
 	}
